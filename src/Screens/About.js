ae36f763c2ff2bf075f3c8f96711f44f42c0ed24
KRISH KAPADIA <kpkapadia@enacton.com>
2021-04-01 15:13:39 +0530

    google signin done

--- src/Screens/About.js
+++ src/Screens/About.js
@@ -1,11 +1,43 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { StyleSheet, Text, View, Button } from 'react-native'
+import {
+    GoogleSignin,
+    GoogleSigninButton,
+    statusCodes,
+} from '@react-native-google-signin/google-signin';
 
 const About = ({ navigation }) => {
+
+    useEffect(() => {
+
+        GoogleSignin.configure({});
+    }, [])
+
+    const signIn = async () => {
+        try {
+            await GoogleSignin.hasPlayServices();
+            const userInfo = await GoogleSignin.signIn();
+            // this.setState({ userInfo });
+            console.log("UserInfo:", userInfo);
+        } catch (error) {
+            console.log("Error:", error);
+            if (error.code === statusCodes.SIGN_IN_CANCELLED) {
+                // user cancelled the login flow
+            } else if (error.code === statusCodes.IN_PROGRESS) {
+                // operation (e.g. sign in) is in progress already
+            } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
+                // play services not available or outdated
+            } else {
+                // some other error happened
+            }
+        }
+    };
+
     return (
         <View>
             <Text>About Screen</Text>
             <Button title="Home" onPress={() => navigation.navigate("Home")} ></Button>
+            <Button title="GoogleSignin" onPress={() => signIn()} ></Button>
 
         </View>
     )
